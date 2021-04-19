5c84f70ea6517bbb1492d86d90fcdf62d11bb5b4
baldo-vela <baldo.vela+github@gmail.com>
2021-04-19 14:13:09 -0500

    Basic user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
+    #has_secure_password
+
+    
+
 end
