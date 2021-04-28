c94a91b1f7f02bf5686320f7650182d1ddd3e46b
baldo-vela <baldo.vela+github@gmail.com>
2021-04-28 17:13:01 -0500

    migration changes and example documents

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,18 @@
 class Track < ApplicationRecord
+
+    def CreateFromSpotify()
+        #Check if it exists in DB
+        #Else make the API call
+            #RSpotify Get a track
+            #pause if error in fetching
+                #binding pry    
+                #assign artist name
+                #assign track name
+                #assign preview url (WIP)
+                #assign the spotify e-id
+                
+
+
+    end
+
 end
