112a0e421c404e228b03e4a436d38c1b835c1575
baldo-vela <baldo.vela+github@gmail.com>
2021-04-30 12:16:55 -0500

    foreign keying playlist to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     has_secure_password
 
     # user has many playlists
-    has_many :playlists
+    has_many :playlists, foreign_key: "user_id", class_name: "Playlist" 
     has_many :tracks, thorough: :playlists 
     # a playlist has many tracks
 end
