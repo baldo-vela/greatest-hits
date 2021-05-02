766a465df02f7b214d9b2f52bc011c0a69610489
baldo-vela <baldo.vela+github@gmail.com>
2021-05-02 15:31:49 -0500

    playlist controller needs the spofiy importing

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -11,5 +11,20 @@ class PlaylistsController < ApplicationController
         @playlist = Playlist.new
     end
 
+    def create
+        #WIP
+        @playlist = Playlist.new(playlist_params)
+        if @playlist.save
+            redirect_to playlist_path(@playlist)
+        else
+            render :new
+        end
+    end
+
+    #-----Helpers & Private Methods------
+    private
+    def playlist_params
+        # params.require(:playlist).permit
+    end
     
 end
