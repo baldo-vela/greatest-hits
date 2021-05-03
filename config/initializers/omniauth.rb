ed9974b4b891efd321a3da2c5c0ddc13bbbdff62
baldo-vela <baldo.vela+github@gmail.com>
2021-05-03 14:54:23 -0500

    requesting additiona permissions from spotify

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,11 @@
 #Usage of the gem is very similar to other OmniAuth strategies. Uses the shimmed keys from Figaro.rail
+#Each scope value is a requested permission as per https://developer.spotify.com/documentation/general/guides/scopes/
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :spotify, Rails.application.credentials.spotify(ENV["spotify_id"]), Rails.application.credentials.spotify(ENV["spotify_secret"]), scope: %w(
       playlist-read-private
       user-read-private
       user-read-email
+      user-top-read
+      user-library-read
     ).join(' ')
   end
