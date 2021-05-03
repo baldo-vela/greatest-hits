b6bdc5275d3ebc408afcfb016571ebbdaa95abaf
baldo-vela <baldo.vela+github@gmail.com>
2021-05-02 22:45:49 -0500

    Holy hell are we damn nearly done? probably not

--- db/migrate/20210420161958_create_playlists.rb
+++ db/migrate/20210420161958_create_playlists.rb
@@ -1,11 +1,11 @@
 class CreatePlaylists < ActiveRecord::Migration[6.0]
   def change
     create_table :playlists do |t|
-      t.string :name
-      t.integer :user_id
-      t.string :description
-      t.string :spotifyPlaylistID
-      t.string :spotifyUserID
+      t.string :name              #Playlist Name Concurrent with Spotify Name
+      t.string :user_id           #Local Application User ID
+      t.string :description       #Also pulled from Spotify
+      t.string :spotifyPlaylistID #Spotify's unique ID 
+      t.string :spotifyUserID     #Spotify Unique User ID
       t.integer :followers
 
       t.integer :likes, default: 0 # Local up-votes
