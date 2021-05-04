2e7623817fb89c2a43a88826186dfea2da743492
baldo-vela <baldo.vela+github@gmail.com>
2021-05-04 13:08:11 -0500

    fixed authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 #Usage of the gem is very similar to other OmniAuth strategies. Uses the shimmed keys from Figaro.rail
 #Each scope value is a requested permission as per https://developer.spotify.com/documentation/general/guides/scopes/
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :spotify, Rails.application.credentials.spotify(ENV["spotify_id"]), Rails.application.credentials.spotify(ENV["spotify_secret"]), scope: %w(
+    provider :spotify,ENV["spotify_id"] , ENV["spotify_secret"], scope: %w(
       playlist-read-private
       user-read-private
       user-read-email
