6dea8cb9bb016d354c919b6655c84bae761b78d9
baldo-vela <baldo.vela+github@gmail.com>
2021-05-04 01:09:13 -0500

    testing OA links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
     include ApplicationHelper
+
+    private
+
+    def redirect_if_not_logged_in
+        redirect_to login_path if !logged_in?
+    end
 end
