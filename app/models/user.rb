72b0a80f1fa3687866cb6a8d503621d166629644
baldo-vela <baldo.vela+github@gmail.com>
2021-05-06 18:34:54 -0500

    comments foundation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
 
     # user has many playlists
     has_many :playlists, foreign_key: "user_id", class_name: "Playlist" 
+    has_many :comments
     # has_many :tracks, thorough: :playlists 
     # a playlist has many tracks
+
+    def playlists_from_spotify
+        
+    end
 end
--- db/migrate/20210420161958_create_playlists.rb
+++ db/migrate/20210420161958_create_playlists.rb
@@ -1,16 +1,16 @@
 class CreatePlaylists < ActiveRecord::Migration[6.0]
   def change
     create_table :playlists do |t|
-      t.string :name              #Playlist Name Concurrent with Spotify Name
-      t.string :user_id           #Local Application User ID
-      t.string :description       #Also pulled from Spotify
-      t.string :spotifyPlaylistID #Spotify's unique ID 
-      t.string :spotifyUserID     #Spotify Unique User ID
+      t.string :name                #Playlist Name Concurrent with Spotify Name
+      t.string :user_id             #Local Application User ID
+      t.string :description         #Also pulled from Spotify
+      t.string :spotifyPlaylistID   #Spotify's unique ID 
+      t.string :spotifyUserID       #Spotify Unique User ID
       t.integer :followers
 
-      t.integer :likes, default: 0 # Local up-votes
-      t.integer :dislikes, default: 0 #Local down-votes
-      t.integer :views # Local views
+      t.integer :likes, default: 0    # Local up-votes
+      t.integer :dislikes, default: 0 # Local down-votes
+      t.integer :views                # Local views
 
       t.timestamps
     end
