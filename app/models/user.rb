912df98127c57335b6d04d5cbf915a5dfb4db8e6
baldo-vela <baldo.vela+github@gmail.com>
2021-05-14 19:02:29 -0500

    adds more documentation to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
 
     # user has many created playlists
     has_many :user_playlists, foreign_key: "user_id", class_name: "Playlist" 
-
+    #This uses ActiveRecord to create an #user_playlist
+    #Going to use this on the User #show page to list playlists they own 
     has_many :comments
     has_many :playlists, through: :comments
     # user has many playlists that they have left a comment on
+    
     # has_many :tracks, thorough: :playlists 
     # a playlist has many tracks
 
