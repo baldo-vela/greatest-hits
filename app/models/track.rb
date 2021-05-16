57ff9158ca8176450e4dc764a11b5702fb821d37
baldo-vela <baldo.vela+github@gmail.com>
2021-05-16 13:39:01 -0500

    adds validations and creation methods to tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,35 +3,47 @@ class Track < ApplicationRecord
     belongs_to :playlist
 
     # Validations
-        # track name
-        # spotify e-id 
+    validates :name, presence: true
+        #Prevents blank playlist names
+    validates :spotify_id, presence: true
+        #must have a spotify ID, which WIP can be used to pull additional information from RSpotify later if needed.
 
     # Scopes
     scope :search, -> (query) { self.where("name LIKE ?", "%#{query}%") }
 
-    def self.new_from_spotify(spotify_track)
+    def self.new_from_spotify(spotify_track, playlist_id = nil)
         #pass in a RSpotify track object to instance this class
+=begin  "name"
+        "spotify_id"
+        "artists"
+        "img"
+        "url"
+        "preview" 
+=end
         Track.new(
             name: spotify_track.name,
             spotify_id: spotify_track.id,
             artists: spotify_track.artists[0].name,
             img: spotify_track.album.images[0]["url"],
+            url: spotify_track.external_urls[0],
             preview: spotify_track.preview_url)  
     end
 
-    def self.create_from_spotify(spotify_track)
+    def self.create_from_spotify(spotify_track, playlist_id = nil)
         track = self.new_from_spotify(spotify_track)
         track.save
         return track
     end
 
-    def self.new_from_playlist(playlist)
+    def self.newTracks_from_playlist(playlist)
         #Take passed playlist hash create local track objects
-
-
+        playlist.tracks.each do |track|
+            Track.new_from_spotify(track)
+        end
     end
 
-    def self.create_from_playlist(playlist)
+    def self.createTracks_from_playlist(playlist)
+        self.newTracks_from_playlist(playlist)
     end
 
 end
