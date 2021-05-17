34f65c28da7db93f94141bde4393b50a084efd37
baldo-vela <baldo.vela+github@gmail.com>
2021-05-16 19:36:28 -0500

    adds nested routes for users associated objects

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,11 @@ Rails.application.routes.draw do
     resources :tracks, only: [:index]
   end
 
-  resources :users
+  resources :users do
+    resources :comments, only: [:index, :delete]
+    #just index and deletion for now, editing will probably be better handled with JS framework
+    resources :playlists, only: [:index,:show, :new, :delete]
+    #show all user_playlists, show an individual one, allow them to create new ones, and delete existing lists they own
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
